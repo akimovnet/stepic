593972c8530cabff71f6ddb2e63c343459a7d136
Konstantin Akimov <konst.akimov@gmail.com>
2016-03-12 01:07:35 +0300

    Добавлены модели Question и Answer

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Question - вопрос
+#title - заголовок вопроса
+#text - полный текст вопроса
+#added_at - дата добавления вопроса
+#rating - рейтинг вопроса (число)
+#author - автор вопроса
+#likes - список пользователей, поставивших "лайк"
+#
+#Answer - ответ
+#text - текст ответа
+#added_at - дата добавления ответа
+#question - вопрос, к которому относится ответ
+#author - автор ответа
+
+class Question(models.Model):
+    title = models.CharField()
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User)
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.OneToOneField(Question)
+    author = models.ForeignKey(User)
